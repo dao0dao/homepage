3e36a03a63210b02293a9931175c7a8a13da31c1
Demid <d_greshnikov@tlen.pl>
2023-07-29 22:03:11 +0200

    add animations and scroll

--- src/app/portfolio/portfolio-pop-up/portfolio-pop-up.component.ts
+++ src/app/portfolio/portfolio-pop-up/portfolio-pop-up.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { Project } from 'src/app/interfaces';
 import { animations } from './animations';
 
@@ -8,16 +15,35 @@ import { animations } from './animations';
   styleUrls: ['./portfolio-pop-up.component.scss'],
   animations: animations,
 })
-export class PortfolioPopUpComponent {
+export class PortfolioPopUpComponent implements OnChanges {
   @Input() project!: Project;
   @Output() Close = new EventEmitter<unknown>();
+  @Output() Scroll = new EventEmitter<'left' | 'right'>();
   isClosing: boolean = false;
+  cardState: boolean = false;
   private readonly animationTime = 300;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (
+      changes['project'].previousValue?.name &&
+      changes['project'].previousValue?.name !==
+        changes['project'].currentValue?.name
+    ) {
+      this.cardState = !this.cardState;
+    }
+  }
 
   close() {
     this.isClosing = true;
     setTimeout(() => {
       this.Close.emit();
     }, this.animationTime);
+  }
+
+  next() {
+    this.Scroll.emit('right');
+  }
+  previous() {
+    this.Scroll.emit('left');
   }
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -11,14 +11,33 @@ export class PortfolioComponent {
   projects: Project[] = myProjects;
   isPopUp: boolean = false;
   showedProject: Project | undefined = undefined;
+  private showedProjectIndex: number = 0;
 
-  showPopUp(project: Project) {
+  showPopUp(project: Project, index: number) {
     this.showedProject = project;
+    this.showedProjectIndex = index;
     this.isPopUp = true;
   }
 
   closePopUp() {
     this.isPopUp = false;
     this.showedProject = undefined;
+    this.showedProjectIndex = 0;
+  }
+
+  scrollPopUp(direction: 'left' | 'right') {
+    if ('right' === direction) {
+      this.showedProjectIndex++;
+      if (this.showedProjectIndex >= this.projects.length) {
+        this.showedProjectIndex = 0;
+      }
+    }
+    if ('left' === direction) {
+      this.showedProjectIndex--;
+      if (this.showedProjectIndex < 0) {
+        this.showedProjectIndex = this.projects.length - 1;
+      }
+    }
+    this.showedProject = this.projects[this.showedProjectIndex];
   }
 }
