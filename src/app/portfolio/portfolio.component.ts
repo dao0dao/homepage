8697bee20599b31a436a49be2c3978d5c69ae898
Demid <d_greshnikov@tlen.pl>
2023-07-30 22:06:01 +0200

    add scrolling to gallery

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,13 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { Project } from '../interfaces';
 import { myProjects } from './projects';
+
+type Direction = 'left' | 'right';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,12 +15,28 @@ import { myProjects } from './projects';
   styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
+  @ViewChild('carContainer') cardsContainerDOM:
+    | ElementRef<HTMLElement>
+    | undefined;
   projects: Project[] = myProjects;
   isPopUp: boolean = false;
   showedProject: Project | undefined = undefined;
+  isLeftBtn: boolean = false;
+  isRightBtn: boolean = false;
   private showedProjectIndex: number = 0;
+  private isScrolling: boolean = false;
+
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.isBtnHidden();
+    this.cd.detectChanges();
+  }
 
   showPopUp(project: Project, index: number) {
+    if (this.isScrolling) {
+      return;
+    }
     this.showedProject = project;
     this.showedProjectIndex = index;
     this.isPopUp = true;
@@ -25,7 +48,7 @@ export class PortfolioComponent {
     this.showedProjectIndex = 0;
   }
 
-  scrollPopUp(direction: 'left' | 'right') {
+  scrollPopUp(direction: Direction) {
     if ('right' === direction) {
       this.showedProjectIndex++;
       if (this.showedProjectIndex >= this.projects.length) {
@@ -39,5 +62,58 @@ export class PortfolioComponent {
       }
     }
     this.showedProject = this.projects[this.showedProjectIndex];
+  }
+
+  scroll(direction: Direction) {
+    const container = this.cardsContainerDOM?.nativeElement;
+    if (!container) {
+      return;
+    }
+    if ('right' === direction) {
+      container.scrollLeft = container.scrollLeft + container.clientWidth;
+    }
+    if ('left' === direction) {
+      container.scrollLeft = container.scrollLeft - container.clientWidth;
+    }
+  }
+
+  startScroll(e: PointerEvent) {
+    e.stopPropagation();
+    this.isScrolling = true;
+  }
+
+  stopScroll(e: PointerEvent) {
+    e.stopPropagation();
+    this.isScrolling = false;
+  }
+
+  moveGallery(event: MouseEvent) {
+    if (!this.isScrolling) {
+      return;
+    }
+    const container = this.cardsContainerDOM?.nativeElement;
+    if (!container) {
+      return;
+    }
+    container.scrollLeft = container.scrollLeft - event.movementX * 10;
+  }
+
+  isBtnHidden(): boolean | void {
+    const container = this.cardsContainerDOM?.nativeElement;
+    if (container) {
+      if (container.scrollLeft === 0) {
+        this.isLeftBtn = false;
+      } else {
+        this.isLeftBtn = true;
+      }
+      if (
+        container.scrollLeft ===
+        container.scrollWidth - container.clientWidth
+      ) {
+        this.isRightBtn = false;
+      } else {
+        this.isRightBtn = true;
+      }
+    }
   }
 }
